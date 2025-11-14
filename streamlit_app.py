d599e8bbc3ea074a2fec5153640830d8920d0836
lukekubehl <lkubehl@mckinneytexas.org>
2025-11-14 16:02:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,9 +14,10 @@ st.write('The name on your Smoothie will be:', name_on_order)
 # --- Step 2: Fetch fruit options from Snowflake
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
-# --- Step 3: Let user choose fruits
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
     my_dataframe,
